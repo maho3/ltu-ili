16142475f8e8a4899ec5d047c7d3d221a94ed855
Matthew Ho <matthewho@macbookho.local>
2022-11-01 17:43:42 +0100

    skeleton code for data objects. notes from hack session 1/11/22

---
+++ ltu-ili/dataops/lightcone.py
@@ -0,0 +1,21 @@
+class Cosmology():
+    # import from astropy/CAMB?
+
+
+class LightCone():
+    def __init__(...):
+        metadata = {
+            cosmoparams: Cosmology(),
+
+
+        },
+
+        galaxies = pd.DataFrame()
+
+class FullLightCone(LightCone):
+
+
+class SurveyLightCone(LightCone):
+    mask_meta = {
+
+    }
---
+++ ltu-ili/dataops/summaries.py
@@ -0,0 +1,17 @@
+class Summaries():
+    def __init__():
+        metadata = {}
+
+        summaries = pd.DataFrame # 
+        labels = pd.DataFrame # 
+
+        nuisance_params = pd.DataFrame # e.g. HOD parameters
+
+        properties = pd.DataFrame # e.g. simulation ID
+
+    def save()
+
+    def load()
+
+class PowerSpectrum(Summaries):
+
