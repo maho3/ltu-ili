964892c4279b59654a26c7d4404cf438578e54da
Carol Cuesta <cuestalz@mit.edu>
2022-11-24 09:09:47 -0500

    updated docstrings

--- ili/inference/loaders.py
+++ ili/inference/loaders.py
@@ -60,7 +60,7 @@ class StaticNumpyLoader(BaseLoader):
         """
         return self.theta
 
-
+    
 class SummarizerDatasetLoader(BaseLoader):
     def __init__(
         self,
@@ -71,11 +71,20 @@ class SummarizerDatasetLoader(BaseLoader):
         train_test_split_file: str,
         param_names: List[str],
     ):
-        """Class to load netCF files of summaries and a csv of parameters
+        """ Class to load netCF files of summaries and a csv of parameters
         Basically a wrapper for ili-summarizer's Dataset, with added parameter loading
 
+
         Args:
-            data_dir (str): path to the location of stored data
+            stage (str): whether to load train, test or val data 
+            data_dir (str): path to data directory
+            summary_root_file (str): root of summary files
+            param_file (str): parameter file name
+            train_test_split_file (str): file name where train, test, val split idx are stored
+            param_names (List[str]): parameters to fit 
+
+        Raises:
+            Exception: won't work when summaries and parameters don't have same length
         """
         self.data_dir = Path(data_dir)
         self.nodes = self.get_nodes_for_stage(
@@ -119,15 +128,34 @@ class SummarizerDatasetLoader(BaseLoader):
         """
         return self.theta
 
-    def get_nodes_for_stage(self, stage: str, train_test_split_file: str):
+    def get_nodes_for_stage(self, stage: str, train_test_split_file: str)->List[int]:
+        """ Get nodes for a given stage (train, test or val)
+
+        Args:
+            stage (str): either train, test or val
+            train_test_split_file (str): file where node idx for each stage are stored 
+
+        Returns:
+            List[int]: list of idx for stage
+        """
         with open(self.data_dir / train_test_split_file) as f:
             train_test_split = json.load(f)
         return train_test_split[stage]
 
     def load_parameters(
-        self, data_dir: Path, param_file: str, nodes: List[int], param_names: List[str]
+        self, param_file: str, nodes: List[int], param_names: List[str]
     ) -> np.array:
-        theta = pd.read_csv(data_dir / param_file, sep=" ", skipinitialspace=True).iloc[
+        """ Get parameters for nodes
+
+        Args:
+            param_file (str): where to find parameters of latin hypercube 
+            nodes (List[int]): list of nodes to read 
+            param_names (List[str]): parameters to use 
+
+        Returns:
+            np.array: array of parameters 
+        """
+        theta = pd.read_csv(self.data_dir / param_file, sep=" ", skipinitialspace=True).iloc[
             nodes
         ]
         return theta[param_names].values
