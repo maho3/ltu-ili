c3d1d00209c5e44e40b39532497a36f020a0b77a
Simon <42298902+AsianTaco@users.noreply.github.com>
2022-12-20 21:47:52 +0100

    Feature/mdn for sbi (#30)
    * Added pytorch mixture density model
    
    * Added mdn option to inference example script
    
    * Revert "Added pytorch mixture density model"
    
    This reverts commit 45c2b15f88cef9553e301625800c1fcadb1d9728.
    
    * Updated documentation
    
    * Adding where to find Quijote TPCF summaries.
    
    * typos
    
    * adding functionality to specify config file path in quijote_inference.py
    
    Co-authored-by: Matthew Ho <matthewho@macbookho.local>

--- INSTALL.md
+++ INSTALL.md
@@ -61,3 +61,14 @@ You can verify that the installation is working by running the toy example
 ```
 After the script completed, you should be able to find some metric summaries and plots in the **examples/toy** folder.
 
+It's also possible to use pre-processed Quijote two point correlation function (TPCF) summaries and run example cosmological inference. Quijote TPCF summaries can be found in the LtU Open Storage Network data repository ([https://sdsc.osn.xsede.org/learningtheuniverse](https://sdsc.osn.xsede.org/learningtheuniverse)). See the download instructions in the #ili-wg Slack channel or ping Matt Ho if you need help getting this data. 
+
+Store this data in a subfolder called ili-wg/data and run the inference using
+```bash
+   cd examples
+   python quijote_inference.py 
+```
+In case you want to use a mixture density network as density estimator instead of a normalizing flow, then execute
+```bash
+   python quijote_inference.py --cfgtrain configs/sample_quijoteTPCF_mdn_config.yaml
+```
--- examples/quijote_inference.py
+++ examples/quijote_inference.py
@@ -1,13 +1,25 @@
-import os
-import numpy as np
+import argparse
+
 from ili.inference.runner_sbi import SBIRunner
 from ili.validation.runner import ValidationRunner
 
 
-# train a model to infer x -> theta. save it as toy/posterior.pkl
-runner = SBIRunner.from_config("configs/sample_quijoteTPCF_config.yaml")
-runner()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Run SBI inference for quijote test data.")
+    parser.add_argument("--cfgtrain", type=str,
+                        default="configs/sample_quijoteTPCF_config.yaml",
+                        help="Configuration file to use for inference training")
+    parser.add_argument("--cfgval", type=str,
+                        default="configs/sample_quijotevalidation_config.yaml",
+                        help="Configuration file to use for inference validation")
 
-# use the trained posterior model to predict on a single example from the test set
-valrunner = ValidationRunner.from_config("configs/sample_quijotevalidation_config.yaml")
-valrunner()
+    args = parser.parse_args()
+
+    # train a model to infer x -> theta. save it as toy/posterior.pkl
+    runner = SBIRunner.from_config(args.cfgtrain)
+    runner()
+
+    # use the trained posterior model to predict on a single example from the test set
+    valrunner = ValidationRunner.from_config(args.cfgval)
+    valrunner()
