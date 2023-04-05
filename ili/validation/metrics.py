834aaf48735a20803e6f369220d86a2f63aa49a8
Pablo-Lemos <plemos91@gmail.com>
2023-04-05 09:59:39 -0400

    First attempt at adding TARP validation metric.

--- ili/validation/metrics.py
+++ ili/validation/metrics.py
@@ -8,6 +8,7 @@ import tqdm
 from sbi.inference.posteriors.base_posterior import NeuralPosterior
 from typing import List
 from pathlib import Path
+import tarp
 
 
 class BaseMetric(ABC):
@@ -22,6 +23,55 @@ class BaseMetric(ABC):
             x (torch.Tensor): tensor of test summaries
             y (torch.Tensor): tensor of test parameters
         """
+
+class TARP(BaseMetric):
+    def __init__(self,
+                 num_samples: int,
+                 labels: List[str],
+                 output_path: Path
+                 ):
+        """Compute the TARP validation metric (https://arxiv.org/abs/2302.03026).
+
+        Args:
+            num_samples (int): number of posterior samples
+            labels (List[str]): list of parameter names
+            output_path (Path): path where to store outputs
+        """
+        self.num_samples = num_samples
+        self.labels = labels
+        self.output_path = output_path
+
+    def __call__(self,
+                 posterior: NeuralPosterior,
+                 x: torch.Tensor,
+                 theta: torch.Tensor,
+                 references: str = "random",
+                 metric: str = "euclidean"
+                 ):
+        """Given a posterior and test data, compute the TARP metric and save to file.
+
+        Args:
+            posterior (NeuralPosterior): trained sbi posterior inference engine
+            x (torch.Tensor): tensor of test summaries
+            theta (torch.Tensor): tensor of test parameters
+            references (str, optional): how to select the reference points. Defaults to "random".
+            metric (str, optional): which metric to use. Defaults to "euclidean".
+        """
+        ndim = theta.shape[-1]
+        num_samples = self.num_samples
+
+        # sample from the posterior
+        samples = posterior.sample((num_samples,), x=x)
+        alpha, ecp = tarp.get_drp_coverage(samples, theta, references=references, metric=metric)
+
+        # plot the TARP metric
+        fig, ax = plt.subplots(1, 1, figsize=(4, 4))
+        ax.plot([0, 1], [0, 1], ls='--', color='k')
+        ax.plot(alpha, ecp, label='DRP')
+        ax.legend()
+        ax.set_ylabel("Expected Coverage")
+        ax.set_xlabel("Credibility Level")
+        plt.savefig(self.output_path / "plot_tarp.jpg")
 
 
 class PlotSinglePosterior(BaseMetric):
@@ -53,7 +103,7 @@ class PlotSinglePosterior(BaseMetric):
         Args:
             posterior (NeuralPosterior): trained sbi posterior inference engine
             x (torch.Tensor): tensor of test summaries
-            y (torch.Tensor): tensor of test parameters
+            theta (torch.Tensor): tensor of test parameters
         """
         ndim = theta.shape[-1]
 
