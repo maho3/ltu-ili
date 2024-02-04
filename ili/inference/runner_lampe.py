adding jacobian normalization to lampe. fixing train/val loss accumulation bug

ili/inference/runner_lampe.py
@@ -83,7 +83,7 @@ class LampeRunner():
             self.signatures = [""]*len(self.nets)
         self.train_args = dict(
             training_batch_size=50, learning_rate=5e-4,
-            stop_after_epochs=20, clip_max_norm=5,
+            stop_after_epochs=30, clip_max_norm=5,
             max_epochs=int(1e10),
             validation_fraction=0.1)
         self.train_args.update(train_args)
@@ -189,33 +189,39 @@ class LampeRunner():
         loss = NPELoss(model)
         model.train()
 
-        loss_train = []
+        loss_train, count = [], 0
         for x, theta in train_loader:
             x, theta = x.to(self.device), theta.to(self.device)
-            loss_train.append(stepper(loss(theta, x)))
-        loss_train = torch.stack(loss_train).mean().item()
+            loss_train.append(stepper(loss(theta, x)) * len(theta))
+            count += len(theta)
+        loss_train = torch.stack(loss_train).sum().item()/count
+
         model.eval()
         with torch.no_grad():
-            loss_val = []
+            loss_val, count = [], 0
             for x, theta in val_loader:
                 x, theta = x.to(self.device), theta.to(self.device)
-                loss_val.append(loss(theta, x))
-            loss_val = torch.stack(loss_val).mean().item()
+                loss_val.append(loss(theta, x) * len(theta))
+                count += len(theta)
+            loss_val = torch.stack(loss_val).sum().item()/count
         return loss_train, loss_val
 
     def _train_round(self, models: List[Callable],
                      train_loader: DataLoader, val_loader: DataLoader):
         """Train a single round of inference for an ensemble of models."""
 
-        posteriors, summaries = [], []
-        for i, model in enumerate(models):
-            logging.info(f"Training model {i+1} / {len(models)}.")
+        # initialize models
+        x_, y_ = next(iter(train_loader))
+        if self.device is not None:
+            x_, y_ = x_.to(self.device), y_.to(self.device)
+        models_rnd = [
+            model(x_, y_, self.prior).to(self.device)
+            for model in models
+        ]
 
-            # initialize model
-            x_, y_ = next(iter(train_loader))
-            if self.device is not None:
-                x_, y_ = x_.to(self.device), y_.to(self.device)
-            model = model(x_, y_, self.prior).to(self.device)
+        posteriors, summaries = [], []
+        for i, model in enumerate(models_rnd):
+            logging.info(f"Training model {i+1} / {len(models_rnd)}.")
 
             # define optimizer
             optimizer = torch.optim.Adam(
@@ -229,7 +235,8 @@ class LampeRunner():
             best_val = float('inf')
             wait = 0
             summary = {'training_log_probs': [], 'validation_log_probs': []}
-            with tqdm(iter(range(self.train_args["max_epochs"])), unit=' epochs') as tq:
+            with tqdm(iter(range(self.train_args["max_epochs"])),
+                      unit=' epochs') as tq:
                 for epoch in tq:
                     loss_train, loss_val = self._train_epoch(
                         model=model,
@@ -255,7 +262,8 @@ class LampeRunner():
                         wait += 1
                 else:
                     logging.warning(
-                        f"Training did not converge in {self.train_args['max_epochs']} epochs.")
+                        "Training did not converge in "
+                        f"{self.train_args['max_epochs']} epochs.")
                 summary['best_validation_log_prob'] = -best_val
                 summary['epochs_trained'] = epoch
 

ili/utils/ndes_pt.py
@@ -113,10 +113,14 @@ class LampeNPE(nn.Module):
         if isinstance(x, (list, np.ndarray)):
             x = torch.Tensor(x)
         x = x.to(self._device)
-        # sample
-        return self.nde(
+
+        logprob = self.nde(
             self.theta_transform.inv(theta),
             self.embedding_net(self.x_transform.inv(x)))
+        log_abs_det_jacobian = self.theta_transform.log_abs_det_jacobian(
+            theta, theta  # just for shape
+        )  # for Affine/IdentityTransform, this outputs a constant
+        return logprob - log_abs_det_jacobian
 
     def flow(self, x: torch.Tensor):  # -> Distribution
         return self.nde.flow(
@@ -205,6 +209,7 @@ class LampeEnsemble(nn.Module):
             nde.sample((int(N),), x, show_progress_bars=show_progress_bars)
             for nde, N in zip(self.posteriors, per_model)
         ], dim=0)
+        samples = samples[:num_samples]
         return samples.reshape(*shape, -1)
 
     def log_prob(self, theta: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
@@ -277,12 +282,14 @@ def load_nde_lampe(
         if x_normalize:
             x_mean = x_batch.mean(dim=0)
             x_std = x_batch.std(dim=0)
-            x_transform = AffineTransform(loc=x_mean, scale=x_std)
+            x_transform = AffineTransform(
+                loc=x_mean, scale=x_std, event_dim=1)
 
         if theta_normalize:
             theta_mean = theta_batch.mean(dim=0)
             theta_std = theta_batch.std(dim=0)
-            theta_transform = AffineTransform(loc=theta_mean, scale=theta_std)
+            theta_transform = AffineTransform(
+                loc=theta_mean, scale=theta_std, event_dim=1)
 
         return LampeNPE(
             nde=nde,